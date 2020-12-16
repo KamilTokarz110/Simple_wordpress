20d9b6455ef40bd8b9f9025174a35db0a74647d3
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2020-12-16 11:02:17 +0100

    Add search page

--- wp-content/themes/simple/content-search.php
+++ wp-content/themes/simple/content-search.php
@@ -7,38 +7,48 @@ Template Name: NewsTemp
 */
 ?>
 <?php get_header();?>
-<div class="wrapper">
-<main class ="post">
-    <div class="content">
-        <header class="post__header">
-            <h2 class="post__title">News List</h2>
-        </header>
-    <div class= "post__list">
-<?php
-			
-						$Post = new WP_Query('type=post'); ?>
-
-		<!-- <div class="news__list"> -->
-						<?php if($Post->have_posts()): ?>
-					
-	 		
 
 	
+<div class="wrapper">
+                        <main class ="post">
+                            <div class="content">
+                               
+                                 <header class="post__header">
+                                    <h2 class="post__title">News List</h2>
+                                 </header>
+                            <div class= "post__list">
+                              
+
+					
 	 		
+                              
+                    <div class="post__item" >
 			
-				<?php while($Post->have_posts()) :$Post->the_post();?>
+            <a href="<?php the_permalink();?>"><img class = "news__item-image item__image " src="<?php echo the_post_thumbnail(); ?>"></a>
+            
+                <div class="item__info">
+                <a class="item__link" href="<?php the_permalink();?>"> <h3 class="item__title"><?php the_title();?></h3></a>
+                    <p class="item__date" >By <span class="item__author"><?php the_author();?></span> on <?php echo get_the_date('d.m.Y');?></p>
+                    <p class="item__description"> <?php echo wp_trim_words( get_the_content(), 20, '<a class="item__link" href="<?php the_permalink();?>"> więcej...</a>' ); ?></p>
+                <!-- <hr> -->
+                    
+                </div>
+                   
+        </div>
+        <hr class="post__line">
 
 		 			
-				
-                
-				<?php endwhile; ?>
+   
+        
+                       
 			
-				<?php endif;
-                wp_reset_postdata();?>
-                 </div>
-			</div>
-			
-		<!-- </div> -->
-                        </main>
+               </div>    
+              </div>
+                            
+                            </main>
                         </div>
+			
+			
+		
+                  
 <?php get_footer(); ?>
--- wp-content/themes/simple/searchform.php
+++ wp-content/themes/simple/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
 <div class="main__search">
-            <input class="main__input main__input--active" type="text" value="" name="s" id="s" type="text" placeholder="<?php the_search_query(); ?>">
+            <input class="main__input main__input--active" type="search" value="<?php echo get_search_query();?>" name="s" id="s" type="text" placeholder="<?php the_search_query(); ?>">
            
             <div class="main__item "><i class="item__image item__image--active fas fa-search"></i></div>
         </div>
